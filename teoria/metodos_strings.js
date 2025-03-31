f12ce22b40f907e0ab89ee6471de2e799252f76e
javier <javier.morabaiz@gmail.com>
2025-03-31 19:33:36 -0300

    actualizacion del repo JS de digital house + Mercado Libre

---
+++ teoria/metodos_strings.js
@@ -0,0 +1,37 @@
+// strings y accesos unicos //
+// let saludo = "hola!"
+// console.log(saludo[4])
+
+// .length  es una propiedad y devuelve total cantidad de caracteres de un string //
+// let miSerie = "Dexter"
+// console.log(miSerie.length)
+
+// .indexOf() //
+// let saludo = "hola! estamos codeando"
+// console.log(saludo.indexOf("o"))
+// console.log(saludo.indexOf("estamos"))
+
+// .slice() //
+// let frase = "hola! chau."
+//  opcion 1 //
+// console.log(frase.slice(6,10))
+//  opcion 2 //
+// console.log(frase.slice(6))
+//  opcion 3 //
+// console.log(frase.slice(-8))
+
+// .trim() elimina espacios al principio y final //
+
+
+
+
+// .split() //
+
+
+
+
+
+
+
+
+
