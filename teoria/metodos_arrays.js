bf91c170cdf5a550db056c0dcf7d95a4e854f539
javier <javier.morabaiz@gmail.com>
2025-03-31 19:10:57 -0300

    actualizacion del repo JS de digital house + Mercado Libre

---
+++ teoria/metodos_arrays.js
@@ -0,0 +1,52 @@
+// .push()  agrega elementos al arreglo//
+// let colores = ["rojo","azul","violeta"]
+// colores.push("gris")
+// console.log(colores)
+// colores.push("blanco","dorado")
+// console.log(colores)
+
+// .pop() elimina el ultimo elemento //
+// let colores = ["rojo","azul","violeta"]
+// let colorEliminado = colores.pop()
+// console.log(colores)
+// console.log(colorEliminado)
+
+
+// .shift() elimina el primer elemento //
+// let colores = ["rojo","azul","violeta"]
+// let colorEliminado = colores.shift()
+// console.log(colores)
+// console.log(colorEliminado)
+
+// .unshift() agrega elementos al principio del arreglo lo opueto a .push() que agrega al final //
+// let motos = ["honda"]
+// motos.unshift("kawa","ducati")
+// console.log(motos)
+
+// .join() une arreglos //
+
+// let dias = ["lunes","martes","miercoles"]
+
+// let diasGuion = dias.join(" - ")
+// console.log(dias)
+// console.log(diasGuion)
+
+// .indexOf() busca el elemento que le indicamos por el parametro//
+// let fruta = ["pera", "kiwi", "sandia"]
+// let indiceEncontrado = fruta.indexOf("sandia")
+// let indiceEncontradoNegativo = fruta.indexOf("anana")
+// console.log(indiceEncontrado)
+// console.log(indiceEncontradoNegativo)
+
+// .lastIndexOf() busca el elementos por el final del arreglo //
+
+// let clubes = ["racing", "boca", "river", "boca"]
+
+// let clubEncontrado = clubes.lastIndexOf("boca")
+// console.log(clubEncontrado)
+
+
+// .includes() retorna booleano en caso de encontrar en el arreglo un elemento //
+
+// let fruta = ["anana","pera","kiwi"]
+// console.log(fruta.includes("pera"))
