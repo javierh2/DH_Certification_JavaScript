d01629320ce2b9b944a39894abdcb3ed7acbcfd8
javier <javier.morabaiz@gmail.com>
2025-03-31 15:53:00 -0300

    actualizacion del repo JS de digital house + Mercado Libre

---
+++ teoria/arrays.js
@@ -0,0 +1,27 @@
+// crear array //
+// let edades = [24, 37, 18, 59, 7]
+
+// itero por cada elemento //
+// for(edad in edades){
+//     console.log(edades[edad])
+// }
+
+// for(let i = 0; i < edades.length; i++){
+//     console.log(edades[i])
+// }
+
+// accedo a un elemento específico //
+// console.log(edades[1])
+
+// modificar cada uno de los elementos de un array //
+
+// let edades = [24, 37, 18, 59, 7]
+
+// for(let i = 0; i < edades.length; i++){
+//     edades[i] = i+1
+// }
+// edades[1] = 100
+// for(let i = 0; i < edades.length; i++){
+//     console.log(edades[i])
+// }
+
