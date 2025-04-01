8b8ba4c4aadbaf6c2103ac46ceda6a3d01d15db6
javier <javier.morabaiz@gmail.com>
2025-03-31 22:20:25 -0300

    actualizacion del repo JS de digital house + Mercado Libre

---
+++ teoria/obj_literales.js
@@ -0,0 +1,31 @@
+// crear objeto //
+let persona = {
+    nombre : "Javier",
+    apelido : "MoraBaiz",
+    edad : 31,
+    gustos : ["bjj", "code"],
+    saludar : function(){
+        return "hola, estoy con JS y soy " + this.nombre;
+    }
+};
+
+// acceder a las propiedades de un obj //
+console.log(persona.nombre,persona.apelido, "tiene",persona.edad,"años y le gusta",persona.gustos[0],"y hacer",persona.gustos[1])
+
+// cargar una nueva propiedad y modificar otra //
+persona.moto = "Honda"
+persona.edad = 30
+// console.log(persona)
+
+// eliminar una nueva propiedad //
+delete persona.edad
+// console.log(persona)
+
+// acceder a un metodo de un obj //
+saludo = persona.saludar()
+// console.log(saludo)
+
+// this .ayuda en acceder a propiedades//
+console.log(saludo)
+persona.nombre = "Juan";
+console.log(saludo)
