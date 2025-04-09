9350c253cc6896e835e5c921da933baacace213b
javier <javier.morabaiz@gmail.com>
2025-04-09 13:47:18 -0300

    actualizacion del repo de JS, proyecto login page

---
+++ proyectos/Login_Page/js/script.js
@@ -0,0 +1,84 @@
+document.addEventListener('DOMContentLoaded', function(){ // escucha el documento apenas se carga la página
+    const loginForm = document.getElementById('loginForm');
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+    const confirmPasswordInput = document.getElementById('confirmPassword');
+    const emailError = document.getElementById('emailError');
+    const passwordError = document.getElementById('passwordError');
+    const confirmPasswordError = document.getElementById('confirmPasswordError');
+    // const showHideButton = document.getElementById('show-hide')
+
+    loginForm.addEventListener('submit', function(event){
+        event.preventDefault()
+        validateForm()
+
+    })
+
+    emailInput.addEventListener('blur',function(){  // blur cuando sale del formulario escucha el evento email
+        validateEmail()
+    })
+
+    emailInput.addEventListener('change',function(){  // change cualquier cambio, limpia el error
+        clearError(emailError)
+    })
+
+    passwordInput.addEventListener('change',function(){  // change cualquier cambio, limpia el error
+        clearError(passwordError)
+    })
+
+    confirmPasswordInput.addEventListener('change',function(){  // change cualquier cambio, limpia el error
+        clearError(confirmPasswordError)
+    })
+
+    function validateForm() {
+        const isValidEmail = validateEmail()
+        const isValidPassword = validatePassword()
+        const passwordMatch = validateMatch()
+
+        if (isValidEmail && isValidPassword && passwordMatch){ // guardo email en localStorage y genero un JSON en consola con los datos
+            alert('estás en la matrix')
+
+        }
+
+    }
+
+    function validateEmail() {
+        const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/
+        const emailValue = emailInput.value.trim()  // limpia espacios vacios al inicio y fin del input
+        if (!emailRegex.test(emailValue)){
+            showError(emailError,'Ingresa un email válido')
+            return false
+        }
+        return true
+    }
+
+    function validatePassword(){
+        const passwordValue = passwordInput.value.trim()
+        if(passwordValue.length < 6){
+            showError(passwordError,'Ingresa una contraseña de 6 o más caracteres')
+            return false
+        }
+        return true
+    }
+
+    function validateMatch() {
+        const passwordValue = passwordInput.value.trim()
+        const confirmPasswordValue = confirmPasswordInput.value.trim()
+        if(passwordValue != confirmPasswordValue){
+            showError(confirmPasswordError,'Las contraseñas no coinciden')
+            return false
+        }
+        return true
+    }
+
+    function showError(errorElement, message) {
+        errorElement.innerHTML = message;
+        errorElement.style.display = 'block';
+    }
+
+    function clearError(errorElement) {
+        errorElement.innerHTML = '';
+        errorElement.style.display = 'none';
+    }
+
+})
